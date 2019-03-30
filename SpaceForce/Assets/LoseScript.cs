2267a9af63e24fcd0427d0d319594d092387f9b7
RahulDeeljore <Rahul_2496@hotmail.com>
2019-03-30 07:52:09 +0200

    game is finished
    what is life

---
+++ SpaceForce/Assets/LoseScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseScript : MonoBehaviour
+{
+    Text lose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lose = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         // 2 lives for multiplayer, 1 for single player
+        if( menu.life == 0)
+        {
+            lose.text = "GAMEOVER";
+        }
+
+    }
+}
